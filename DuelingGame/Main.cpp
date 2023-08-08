770eb6a61eee9165c539311c73fbe1c56e921f04
Zaid Arshad <zaidyarshad@gmail.com>
2023-08-07 19:15:51 -0700

    Having triangle point follow mouse

--- DuelingGame/Main.cpp
+++ DuelingGame/Main.cpp
@@ -1,9 +1,46 @@
-#include<iostream>
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
+#include <GLFW/glfw3.h>
 
-int main()
+int main(void)
 {
-	glfwInit();
-	return 0;
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    double xpos, ypos;
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwGetCursorPos(window, &xpos, &ypos);
+
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2d((xpos-320)/320, (240-ypos)/240);
+        glVertex2d(0.5, 0.5);
+        glVertex2d(-0.5, -0.5);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
