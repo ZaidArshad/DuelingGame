755a2828cf026f7d685ad0454fef12f716455fa0
Zaid Arshad <zaidyarshad@gmail.com>
2023-10-10 02:30:15 -0700

    Refactoring heading calculation

--- DuelingGame/src/Helper/AppTools.cpp
+++ DuelingGame/src/Helper/AppTools.cpp
@@ -4,6 +4,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtx/vector_angle.hpp"
 
 #include <iostream>
 #include <sstream>
@@ -18,25 +19,23 @@ float AppTools::normalizeY(float position)
 	return (WINDOW_HEIGHT/2 - position) / (WINDOW_HEIGHT/2);
 }
 
+// Calculated for 45 degree special triangle
 float AppTools::calculateHeading(float x, float y)
 {
 	double heading = 0;
 	if (x != 0 && y != 0)
 	{
-		heading = glm::atan(x/y);
+		heading = glm::atan(x/-y);
 	}
-	else if (x != 0 || y < 0)
+	// When y is zero
+	else if (x != 0)
 	{
 		heading = glm::half_pi<double>();
 	}
-
-	if (y < 0)
+	// When x is zero
+	else if (y < 0)
 	{
-		heading += (heading > 0) ? glm::half_pi<double>() : -glm::half_pi<double>();
-	}
-	else
-	{
-		heading *= -1;
+		heading = glm::pi<double>();
 	}
 
 	return heading;
